093cebe3f3945270aadc00ab343c657746c3e036
Marshall Bowers <elliott.codes@gmail.com>
2019-06-05 21:47:24 -0400

    Read the dotfiles from disk

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, read_dir};
 use std::path::{Path, PathBuf};
 
 use structopt::StructOpt;
 
 #[derive(Debug)]
 struct Dotfile {
-    name: String,
     path: PathBuf,
 }
 
 impl Dotfile {
+    fn from_path(path: PathBuf) -> Self {
+        Self { path }
+    }
+
     fn name(&self) -> String {
-        self.name.clone()
+        self.path
+            .file_name()
+            .unwrap()
+            .to_os_string()
+            .into_string()
+            .unwrap()
     }
 
     fn dotname(&self) -> String {
-        format!(".{}", self.name)
+        format!(".{}", self.name())
     }
 }
 
@@ -64,10 +72,20 @@ fn main(args: Args) -> Result<(), std::io::Error> {
 
     create_dir_all(&home_directory)?;
 
-    let dotfiles = vec![Dotfile {
-        name: "vimrc".into(),
-        path: Path::new("./examples/dotfiles/vimrc").to_path_buf(),
-    }];
+    let dotfiles = {
+        let mut dotfiles: Vec<Dotfile> = Vec::new();
+
+        for entry in read_dir(args.dotfiles_directory)? {
+            let entry = entry?;
+            let path = entry.path();
+
+            if !path.is_dir() {
+                dotfiles.push(Dotfile::from_path(path));
+            }
+        }
+
+        dotfiles
+    };
 
     for dotfile in dotfiles {
         symlink_dotfile(&home_directory, &dotfile)?;
