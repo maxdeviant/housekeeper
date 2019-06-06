ac76ee46f6718e1bc44c5bdc7c39889bd8b2e879
Marshall Bowers <elliott.codes@gmail.com>
2019-06-05 20:59:46 -0400

    Add ability to specify a custom home directory

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
+use std::fs::create_dir_all;
+use std::path::PathBuf;
+
 use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "housekeeper")]
-struct Args {}
+struct Args {
+    #[structopt(long = "home", parse(from_os_str))]
+    home_directory: Option<PathBuf>,
+}
 
 #[paw::main]
-fn main(_args: Args) -> Result<(), std::io::Error> {
+fn main(args: Args) -> Result<(), std::io::Error> {
+    let home_directory = args
+        .home_directory
+        .or_else(dirs::home_dir)
+        .expect("No home directory set.");
+
+    create_dir_all(home_directory)?;
+
     Ok(())
 }
